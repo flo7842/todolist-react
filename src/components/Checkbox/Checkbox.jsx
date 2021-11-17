a0f26d732e47f23f608028e7154aa029c773ce1b
bracq <florianbracq42@gmail.com>
2021-11-17 21:57:17 +0100

    Remove task from drag event

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import style from './Checkbox.module.css'
 
 const Checkbox = (props) => {
-    console.log('props is checked', props.isChecked)
-    const [toto, setToto] = useState('toto')
+    
     return (
         <div className={style.checkboxSection}>
             <input type="checkbox" onChange={props.clickHandler} checked={props.isChecked} data-testid="custom-element" />
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -8,59 +8,32 @@ import axios from 'axios';
 
 const Task = (props) => {
 
-    const [todo, setTodo] = useState(props.item.status)
+    const [status, setStatus] = useState(props.item.status)
     const [inputUpdate, setInputUpdate] = useState('')
     const [visibleInput, setVisibleInput] = useState(style.inputHidden)
     const [visibleBtn, setVisibleBtn] = useState(style.hiddenBtn)
-    const [todoTitle, setTodoTitle] = useState(style.todoTitle)
+    
+    const [statusTitle, setStatusTitle] = useState(style.statusTitle)
+    
     let userId = props.item.id
     let config = {
         headers: {
           'Authorization': 'Bearer ' + props.authUser.token
         }
-      }
-      //console.log(props.)
-
-    // useEffect(() => {
-        
-    //     console.log('Le premier todo', todo) 
-    // }, [todo])
-
-    
-    const handleClick = async (props) => {
-            console.log('props', props.target.checked)
-
-            await setTodo(!todo)
-            console.log('fonction onchange', todo)
-        
-        try{
-            await axios
-                .put('http://localhost:8000/api/tasks/' + userId, {status: props.target.checked}, config)
-                .then(response => {
-                    console.log(response)
-                })
-        }catch(error){
-            console.log(error)
-        }
     }
 
-
-
-    const handleDblClick = (input) => {
-      
-        //setQuery([...query, {id: query.length + 1, inputText: input, dateDay: dateDay}])
+    const handleClick = async (props) => {
+        await setStatus(!status)   
     }
 
     const handleChange = (e) => {
-        
         setInputUpdate(e.target.value)
-        
     }
 
     const dblClickTask = () => {
         setInputUpdate(props.item.inputText)
         setVisibleInput(style.visibleInput)
-        setTodoTitle(style.todoTitleHidden)
+        setStatusTitle(style.statusTitleHidden)
 
         setVisibleBtn(style.visibleBtn)
     }
@@ -68,9 +41,8 @@ const Task = (props) => {
     const updateTask = () => {
         props.updateTask(props.item.id, inputUpdate)
 
-        
         setVisibleInput(style.inputHidden)
-        setTodoTitle(style.todoTitle)
+        setStatusTitle(style.statusTitle)
         setVisibleBtn(style.hiddenBtn)
     }
 
@@ -79,27 +51,26 @@ const Task = (props) => {
     var n = d.toLocaleString('fr-FR');
     var dateFrSplit = n.split(",")
     var dateFr = dateFrSplit.join(" à")
-    
 
     return (
 
-        <div className={style.taskContainer}>
+        <div>
             { props.item &&
                 props.item.title && props.item.title.length > 1 && props.item.title !== undefined ? 
-                <div className={todo ? style.taskChecked : style.container}>
+                <div className={status ? style.taskChecked : style.container}>
                     <div>
-                        <h1 onDoubleClick={dblClickTask} className={todoTitle}>{props.item.title}</h1>
+                        <h1 onDoubleClick={dblClickTask} className={statusTitle}>{props.item.title}</h1>
                         <InputEdit 
                             dblClickTask={dblClickTask}
                             handleChange={handleChange}
                             visibleInput={visibleInput}
                             updateTask={updateTask}
-                            todoTitleItem={props.item.title}
+                            statusTitleItem={props.item.title}
                             visibleBtn={visibleBtn}
                         />
                         <p className={style.addedAt}>Ajouté le {dateFr}</p>
                     </div>
-                    <Checkbox clickHandler={handleClick} isChecked={todo}/>
+                    <Checkbox clickHandler={handleClick} isChecked={status}/>
                 </div> : 'Aucune tache définis'
             }
         </div>
@@ -107,7 +78,7 @@ const Task = (props) => {
 };
 
 Task.propTypes = {
-    todo: PropTypes.array,
+    status: PropTypes.array,
     inputText: PropTypes.string,
     dateDay: PropTypes.string
 }
